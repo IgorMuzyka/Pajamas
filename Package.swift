63384f9a0f3e508943bc748f8d8f1026b1115488
Igor Muzyka <imuz@ciklum.com>
2018-10-18 13:21:29 +0300

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 		.package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
 		.package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
 
-		.package(url: "https://github.com/Quick/Quick", from: "1.3.0"),
-		.package(url: "https://github.com/Quick/Nimble", from: "7.1.0"),
+		.package(url: "https://github.com/Quick/Quick", from: "1.3.2"),
+		.package(url: "https://github.com/Quick/Nimble", from: "7.3.1"),
 
     ],
     targets: [
